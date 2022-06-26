3c5a3bdadaf4c234b33df1d68499d61d41dcbc93
Cédric Méléard <cedric.meleard@gmail.com>
2022-06-27 00:04:15 +0200

    Update WordRepository.cs
    change file path expecting to be copied in tests using github action (working in local)

--- src/server/Wordle/Wordle/WordRepository.cs
+++ src/server/Wordle/Wordle/WordRepository.cs
@@ -7,7 +7,7 @@ namespace Wordle
         private static Random randomizer = new Random(DateTime.Now.DayOfYear);
         public string GetWord()
         {
-            string wordsFromFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"datas\5_letter_words_FR.txt");
+            string wordsFromFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"datas/5_letter_words_FR.txt");
             var words = File
                 .ReadAllText(wordsFromFile)
                 .Split()
